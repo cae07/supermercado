5ff7bf3ca5e32eee10030406160f2a2b71a8a13e
cae07 <eduardokae01@hotmail.com>
2024-05-08 18:44:20 -0300

    feat: array para tipos de despesas

---
+++ src/arrays.helpers/expensesType.js
@@ -0,0 +1,13 @@
+const expensesType = [
+    'Casa',
+    'Alimentação',
+    'Carro',
+    'Dogs',
+    'Passeios',
+    'Cartão de Crédito',
+    'Farmácia',
+    'Trybe', 
+    'Outros'
+];
+
+export default expensesType;
